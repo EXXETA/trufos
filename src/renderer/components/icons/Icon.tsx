9f6ebdae8eee193c47ca09997d4861e86bacdb9b
Eugen Soliar <eugen.soliar@exxeta.com>
2024-06-25 20:10:29 +0200

    imports SVGs as reusable React components

--- src/renderer/components/icons/Icon.tsx
+++ src/renderer/components/icons/Icon.tsx
@@ -1,20 +1,25 @@
+// src/components/icons/Icon.tsx
+
 import React from 'react';
 
 interface IconProps extends React.SVGProps<SVGSVGElement> {
-  Component: React.FC<React.SVGProps<SVGSVGElement>>;
   size?: number;
   color?: string;
 }
 
-const Icon: React.FC<IconProps> = ({ Component, size = 24, color = 'currentColor', ...props }) => {
+const Icon: React.FC<IconProps> = ({ children, size = 24, color = 'currentColor', viewBox = '0 0 32 32', ...props }) => {
   return (
-      <Component
+      <svg
           width={size}
           height={size}
           fill={color}
+          viewBox={viewBox}
           {...props}
-      />
+      >
+        {children}
+      </svg>
   );
 };
 
 export default Icon;
+
