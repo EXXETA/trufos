26e77a78115467810c0f541f0b0cc6dcf13b5777
Eugen Soliar <eugen.soliar@exxeta.com>
2024-07-07 15:10:40 +0200

    # fixed default viewBox prop

--- src/renderer/components/icons/Icon.tsx
+++ src/renderer/components/icons/Icon.tsx
@@ -3,9 +3,17 @@ import React from 'react';
 interface IconProps extends React.SVGProps<SVGSVGElement> {
   size?: number;
   color?: string;
+  viewBox?: string;
+  children?: React.ReactNode;
 }
 
-const Icon: React.FC<IconProps> = ({ children, size = 24, color = 'currentColor', viewBox = '0 0 32 32', ...props }) => {
+const Icon: React.FC<IconProps> = ({
+  children,
+  size = 24,
+  color = 'currentColor',
+  viewBox = `0 0 ${size*4/3} ${size*4/3}`,
+  ...props
+}) => {
   return (
       <svg
           width={size}
