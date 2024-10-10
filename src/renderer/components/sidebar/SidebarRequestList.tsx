aa4d74275b9b7721f70ef37f5bb39484dcf1b450
SoulKa <louiswilke@gmx.de>
2024-10-10 22:30:34 +0200

    #76 - support saving new requests

--- src/renderer/components/sidebar/SidebarRequestList.tsx
+++ src/renderer/components/sidebar/SidebarRequestList.tsx
@@ -1,15 +1,18 @@
-import {useDispatch, useSelector} from "react-redux";
-import {AppDispatch, RootState} from "@/state/store";
-import {setSelectedRequest, deleteRequest} from "@/state/requestsSlice";
-import {httpMethodColor} from "@/services/StyleHelper";
-import {RufusRequest} from "shim/objects/request";
-import {FaTimes} from 'react-icons/fa'; // Importing cross icon
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/state/store';
+import { deleteRequest, setSelectedRequest } from '@/state/requestsSlice';
+import { httpMethodColor } from '@/services/StyleHelper';
+import { RufusRequest } from 'shim/objects/request';
+import { FaTimes } from 'react-icons/fa'; // Importing cross icon
+import { RendererEventService } from '@/services/event/renderer-event-service';
 
 interface SidebarRequestListProps {
   requests: RufusRequest[];
 }
 
-export const SidebarRequestList = ({ requests = []}: SidebarRequestListProps) => {
+const eventService = RendererEventService.instance;
+
+export const SidebarRequestList = ({ requests = [] }: SidebarRequestListProps) => {
   const dispatch = useDispatch<AppDispatch>();
   const selectedRequest = useSelector((state: RootState) => state.requests.selectedRequest);
 
@@ -18,28 +21,37 @@ export const SidebarRequestList = ({ requests = []}: SidebarRequestListProps) =>
     setSelectedRequest(index);
   };
 
-  const handleDeleteClick = (event: React.MouseEvent, index: number) => {
+  const handleDeleteClick = async (event: React.MouseEvent, index: number) => {
+    const request = requests[index];
+    if (request == null) {
+      return;
+    }
+
     dispatch(deleteRequest(index));
-    console.log(`Request ${index} deleted`);
+    if (request.id != null) {
+      await eventService.deleteObject(request);
+    }
+    console.info('Request deleted', request);
   };
 
   return (
-      <table className="w-full table-auto">
-        <tbody>
-        {requests.map((request, index) => (
-            <tr
-                key={index}
-                className={`cursor-pointer hover:bg-gray-600 ${selectedRequest == index ? 'bg-gray-500' : ''}`}
-                onClick={() => handleRowClick(index)}
-            >
-              <td className={'p-2 font-bold ' + httpMethodColor(request.method)}>{request.method}</td>
-              <td className="p-2">{request.url}</td>
-              <td className="p-2 text-right">
-                <FaTimes onClick={(event) => handleDeleteClick(event, index)} className="cursor-pointer ml-2" />
-              </td>
-            </tr>
-        ))}
-        </tbody>
-      </table>
+    <table className="w-full table-auto">
+      <tbody>
+      {requests.map((request, index) => (
+        <tr
+          key={index}
+          className={`cursor-pointer hover:bg-gray-600 ${selectedRequest == index ? 'bg-gray-500' : ''}`}
+          onClick={() => handleRowClick(index)}
+        >
+          <td className={'p-2 font-bold ' + httpMethodColor(request.method)}>{request.method}</td>
+          <td className="p-2">{request.url}</td>
+          <td className="p-2 text-right">
+            <FaTimes onClick={(event) => handleDeleteClick(event, index)}
+                     className="cursor-pointer ml-2" />
+          </td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
   );
 };
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -6,7 +6,7 @@ import * as monaco from 'monaco-editor';
 import { Provider } from 'react-redux';
 import { store } from '@/state/store';
 import { RendererEventService } from '@/services/event/renderer-event-service';
-import { setRequests } from '@/state/requestsSlice';
+import { initialize } from '@/state/requestsSlice';
 import { RufusRequest } from '../shim/objects/request';
 
 console.info('Initializing renderer process...');
@@ -21,7 +21,7 @@ loader.config({ monaco });
 
 RendererEventService.instance.loadCollection().then(collection => {
   const requests = collection.children.filter(c => c.type === 'request') as RufusRequest[];
-  store.dispatch(setRequests(requests));
+  store.dispatch(initialize({ requests, collectionId: collection.id }));
   root.render(<Provider store={store}><App /></Provider>);
 });
 
