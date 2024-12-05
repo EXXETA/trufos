c871562fd872eec8ecf1c062c5b89837d5d5f70e
SoulKa <louiswilke@gmx.de>
2024-12-05 10:10:48 +0100

    #119 - add SimpleSelect component

---
+++ src/renderer/components/mainWindow/bodyTabs/InputTabs/SimpleSelect.tsx
@@ -0,0 +1,42 @@
+import { RequestBodyType } from '../../../../../shim/objects/request';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { useState } from 'react';
+
+export interface SimpleSelectProps<T> {
+  value: T;
+  onValueChange: (value: T) => void;
+  placeholder?: string;
+  items: [T, string][];
+}
+
+export function SimpleSelect<T extends string>(props: SimpleSelectProps<T>) {
+  const { value, onValueChange, placeholder, items } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Select
+      value={value}
+      onValueChange={(value) => onValueChange(value as T)}
+      onOpenChange={setIsOpen}
+    >
+      <SelectTrigger className={'w-[fit-content] h-[fit-content] p-0 '} isOpen={isOpen}>
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+
+      <SelectContent>
+        <SelectGroup>
+          {items.map(([value, label]) => (
+            <SelectItem value={value}>{label}</SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
