3220f5af822c7ef31688f221c1bc5e948297cdbe
SoulKa <louiswilke@gmx.de>
2024-12-19 14:32:06 +0100

    #160 - add syntax highlighting for template variables in JSON and XML

--- src/renderer/components/shared/settings/monaco-settings.ts
+++ src/renderer/components/shared/settings/monaco-settings.ts
@@ -1,7 +1,7 @@
 import { editor } from 'monaco-editor';
-import IEditorOptions = editor.IEditorOptions;
 
-export const DEFAULT_MONACO_OPTIONS: Partial<IEditorOptions> = {
+export const DEFAULT_MONACO_OPTIONS: Partial<editor.IStandaloneEditorConstructionOptions> = {
   minimap: { enabled: false },
   wordWrap: 'on',
+  'semanticHighlighting.enabled': true,
 };
