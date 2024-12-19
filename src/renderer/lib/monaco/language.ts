9d26324259d382b6b8c149055e34e9fda31af084
SoulKa <louiswilke@gmx.de>
2024-12-19 18:09:51 +0100

    #160 - add automatic suggestions for template variables

--- src/renderer/lib/monaco/language.ts
+++ src/renderer/lib/monaco/language.ts
@@ -1,8 +1,17 @@
 import { languages } from 'monaco-editor';
 import { TemplateVariableSemanticTokensProvider } from '@/lib/monaco/TemplateVariableSemanticTokensProvider';
+import { TemplateVariableCompletionItemsProvider } from '@/lib/monaco/TemplateVariableCompletionItemsProvider';
+
+export enum Language {
+  JSON = 'json',
+  XML = 'xml',
+  TEXT = 'plaintext',
+}
+
+const supportedLanguages = [Language.JSON, Language.XML, Language.TEXT];
 
 // plaintext using fast syntax highlighting
-languages.setMonarchTokensProvider('plaintext', {
+languages.setMonarchTokensProvider(Language.TEXT, {
   tokenizer: {
     root: [[/\{\{\s*(\$?\w+)\s*}}/, 'variable.template']],
   },
@@ -10,6 +19,12 @@ languages.setMonarchTokensProvider('plaintext', {
 
 // other rich languages using semantic tokens
 languages.registerDocumentSemanticTokensProvider(
-  ['json', 'xml'],
+  supportedLanguages.filter((lang) => lang !== Language.TEXT),
   new TemplateVariableSemanticTokensProvider()
+);
+
+// completion items provider (suggests template variables)
+languages.registerCompletionItemProvider(
+  supportedLanguages,
+  new TemplateVariableCompletionItemsProvider()
 );
