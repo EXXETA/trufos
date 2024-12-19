9bc904204656f6883564288bf9b22a09590e0f38
SoulKa <louiswilke@gmx.de>
2024-12-19 14:32:06 +0100

    #160 - add syntax highlighting for template variables in JSON and XML

--- src/renderer/lib/monaco/language.ts
+++ src/renderer/lib/monaco/language.ts
@@ -1,8 +1,15 @@
 import { languages } from 'monaco-editor';
+import { TemplateVariableSemanticTokensProvider } from '@/lib/monaco/TemplateVariableSemanticTokensProvider';
 
-// plaintext
+// plaintext using fast syntax highlighting
 languages.setMonarchTokensProvider('plaintext', {
   tokenizer: {
-    root: [[/\{\{\s*(\$?\w+)\s*}}/, 'variable.other']],
+    root: [[/\{\{\s*(\$?\w+)\s*}}/, 'variable.template']],
   },
 });
+
+// other rich languages using semantic tokens
+languages.registerDocumentSemanticTokensProvider(
+  ['json', 'xml'],
+  new TemplateVariableSemanticTokensProvider()
+);
