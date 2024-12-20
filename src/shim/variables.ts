209dd6f4f9117ecb4cb49f0810742887d8963213
SoulKa <louiswilke@gmx.de>
2024-12-20 13:57:34 +0100

    #160 - support getting all environment variables

--- src/shim/variables.ts
+++ src/shim/variables.ts
@@ -1,7 +1,19 @@
 /** Regex for variable names, e.g. for collection variables */
 export const VARIABLE_NAME_REGEX = /^[a-zA-Z_]+(-?[0-9a-zA-Z_]+)*$/;
 
+/**
+ * A variable object. Either a user defined variable or a system variable.
+ */
 export type VariableObject = {
+  /** The key of the variable. */
+  key: string;
+
+  /** The value of the variable. Might change on each call for dynamic variables */
   value: string;
+
+  /** Whether the variable is enabled. */
   enabled: boolean;
+
+  /** A description of the variable. */
+  description?: string;
 };
