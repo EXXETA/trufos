74923dbb505bacecdb639d75d9b831ec27696549
SoulKa <louiswilke@gmx.de>
2025-01-07 16:36:19 +0100

    #160 - add tests for system variables

---
+++ src/main/environment/service/system-variable.test.ts
@@ -0,0 +1,56 @@
+import { getSystemVariable, getSystemVariableKeys, getSystemVariables } from './system-variable';
+
+describe('getSystemVariable()', () => {
+  it('should return the the system variable object', () => {
+    // Arrange
+    const key = '$randomUuid';
+
+    // Act
+    const result = getSystemVariable(key);
+
+    // Assert
+    expect(result.key).toBe(key);
+    expect(result.enabled).toBe(true);
+    expect(result.value).toBeDefined();
+    expect(result.description).toBeDefined();
+  });
+
+  it('should return a random UUID for $randomUuid', () => {
+    // Arrange
+    const key = '$randomUuid';
+
+    // Act
+    const firstValue = getSystemVariable(key).value;
+    const secondValue = getSystemVariable(key).value;
+
+    // Assert
+    expect(firstValue).not.toBe(secondValue);
+  });
+});
+
+describe('getSystemVariableKeys()', () => {
+  it('should return all system variable keys', () => {
+    // Act
+    const result = getSystemVariableKeys();
+
+    // Assert
+    expect(result).toContain('$randomUuid');
+    expect(result).toHaveLength(6);
+  });
+});
+
+describe('getSystemVariables()', () => {
+  it('should return all system variables', () => {
+    // Act
+    const result = getSystemVariables();
+
+    // Assert
+    expect(result).toContainEqual({
+      key: '$randomUuid',
+      enabled: true,
+      value: expect.any(String),
+      description: expect.any(String),
+    });
+    expect(result).toHaveLength(6);
+  });
+});
