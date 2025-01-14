5f478f3d6a787725361808f5a27ea3597fab48bd
Michael Giek <michael.giek@exxeta.com>
2025-01-14 08:29:44 +0100

    #15 - change to dropdown-menu

---
+++ src/renderer/components/sidebar/SidebarRequestList/ContextMenu/RequestContextMenu.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { CiMenuKebab } from 'react-icons/ci';
+import { useRequestActions } from '@/state/requestStore';
+import { handleMouseEvent } from '@/util/callback-util';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+interface RequestContextMenuProps {
+  index: number;
+}
+
+export const RequestContextMenu = ({ index }: RequestContextMenuProps) => {
+  const { deleteRequest } = useRequestActions();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <CiMenuKebab className="cursor-pointer hover:fill-gray-900" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className={'bg-background'}>
+        <DropdownMenuItem onClick={handleMouseEvent(() => deleteRequest(index))}>
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
