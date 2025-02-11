3d67f56e59262986b319c79f38d57ff7873da6fb
SoulKa <louiswilke@gmx.de>
2025-02-11 17:49:02 +0100

    #259 - made variable editor a reusable, stateful component

--- src/renderer/components/shared/settings/SettingsModal.tsx
+++ src/renderer/components/shared/settings/SettingsModal.tsx
@@ -9,19 +9,26 @@ import {
 import { FiSettings } from 'react-icons/fi';
 import { VariableEditor } from '@/components/shared/settings/VariableEditor/VariableEditor';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
-import { useVariableActions, useVariableStore } from '@/state/variableStore';
 import { Button } from '@/components/ui/button';
 import * as React from 'react';
-import { useCallback } from 'react';
+import { useCallback, useRef, useState } from 'react';
+import { useCollectionActions, useCollectionStore } from '@/state/collectionStore';
 
 export const SettingsModal = () => {
-  const { save, cancel, openModal } = useVariableActions();
-  const isOpen = useVariableStore((state) => state.isOpen);
-  const handleOpenChange = useCallback((open: boolean) => !open && cancel(), [cancel]);
+  const variableEditorRef = useRef<VariableEditor>();
+  const [isOpen, setOpen] = useState(false);
+  const { setVariables } = useCollectionActions();
+
+  const variables = useCollectionStore((state) => state.collection.variables);
+  const save = useCallback(() => {
+    if (variableEditorRef.current == null) return;
+    setVariables(variableEditorRef.current.getVariables());
+    setOpen(false);
+  }, [setVariables, setOpen]);
 
   return (
-    <Dialog open={isOpen} onOpenChange={handleOpenChange}>
-      <DialogTrigger onClick={openModal}>
+    <Dialog open={isOpen} onOpenChange={setOpen}>
+      <DialogTrigger onClick={() => setOpen(true)}>
         <FiSettings className="text-xl ml-2" />
       </DialogTrigger>
       <DialogContent style={{ minWidth: '100vh' }}>
@@ -33,14 +40,14 @@ export const SettingsModal = () => {
             <TabsTrigger value="variables">Variables</TabsTrigger>
           </TabsList>
           <TabsContent value="variables" className="max-h-[50vh] overflow-y-auto">
-            <VariableEditor />
+            <VariableEditor initialVariables={variables} ref={variableEditorRef} />
           </TabsContent>
         </Tabs>
-        <DialogFooter className={'bottom-0'}>
+        <DialogFooter className="bottom-0">
           <Button className="mt-0 mr-2 mb-0" onClick={save}>
             <span className="leading-4 font-bold">Save</span>
           </Button>
-          <Button className="mt-0 mr-2 mb-0" onClick={cancel} variant="destructive">
+          <Button className="mt-0 mr-2 mb-0" onClick={() => setOpen(false)} variant="destructive">
             <span className="leading-4 font-bold">Cancel</span>
           </Button>
         </DialogFooter>
--- src/renderer/components/shared/settings/VariableEditor/VariableEditor.tsx
+++ src/renderer/components/shared/settings/VariableEditor/VariableEditor.tsx
@@ -9,85 +9,132 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { useVariableStore } from '@/state/variableStore';
+import { VariableMap, VariableObject } from 'shim/objects/variables';
+import { useImmer } from 'use-immer';
+import { forwardRef, useImperativeHandle } from 'react';
 
-export const VariableEditor = () => {
-  const { addNewVariable, deleteVariable, update, rename } = useVariableStore();
-  const variables = useVariableStore((state) => state.variables);
+export interface VariableEditorProps {
+  initialVariables?: VariableMap;
+}
 
-  return (
-    <div className="p-4 relative">
-      <div className="absolute top-4 right-4 left-4 z-10">
-        <div className="flex">
-          <Button
-            className="hover:bg-transparent gap-1 h-fit"
-            size="sm"
-            variant="ghost"
-            onClick={addNewVariable}
-          >
-            <AddIcon /> Add Variable
-          </Button>
+export interface VariableEditor {
+  getVariables(): VariableMap;
+}
+
+/**
+ * A component that allows adding, deleting, renaming and updating variables using a key-value table
+ * styled editor
+ */
+export const VariableEditor = forwardRef<VariableEditor, VariableEditorProps>(
+  ({ initialVariables }, ref) => {
+    const [variables, updateVariables] = useImmer(initialVariables ?? {});
+
+    // support getting variables on demand
+    useImperativeHandle(ref, () => ({
+      getVariables: () => variables,
+    }));
+
+    function add() {
+      if (variables[''] == null) variables[''] = { value: '' };
+    }
+
+    function rename(oldKey: string, newKey: string) {
+      updateVariables((variables) => {
+        variables[newKey] = variables[oldKey];
+        delete variables[oldKey];
+      });
+    }
+
+    function update<K extends keyof VariableObject>(
+      variableKey: string,
+      property: K,
+      value: VariableObject[K]
+    ) {
+      updateVariables((variables) => {
+        variables[variableKey][property] = value;
+      });
+    }
+
+    function deleteByKey(key: string) {
+      updateVariables((variables) => {
+        delete variables[key];
+      });
+    }
+
+    return (
+      <div className="p-4 relative">
+        <div className="absolute top-4 right-4 left-4 z-10">
+          <div className="flex">
+            <Button
+              className="hover:bg-transparent gap-1 h-fit"
+              size="sm"
+              variant="ghost"
+              onClick={add}
+            >
+              <AddIcon /> Add Variable
+            </Button>
+          </div>
+          <Divider className="mt-2" />
         </div>
-        <Divider className="mt-2" />
-      </div>
 
-      <div className="absolute top-16 left-4 bottom-4 right-4">
-        <Table className="table-auto w-full">
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-auto">Key</TableHead>
-              <TableHead className="w-auto">Value</TableHead>
-              <TableHead className="w-full">Description</TableHead>
-              <TableHead className="w-16">{/* Action Column */}</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {Object.entries(variables).map(([key, variable], index) => (
-              <TableRow key={index}>
-                <TableCell className="w-1/4 break-all">
-                  <input
-                    type="text"
-                    value={key}
-                    className={`w-full bg-transparent outline-none ${key === '' ? 'text-danger' : ''}`}
-                    placeholder="Enter variable key"
-                    onChange={(e) => rename(key, e.target.value)}
-                  />
-                </TableCell>
-                <TableCell className="w-1/4 break-all">
-                  <input
-                    type="text"
-                    value={variable.value}
-                    className="w-full bg-transparent outline-none"
-                    placeholder="Enter variable value"
-                    onChange={(e) => update(key, { value: e.target.value })}
-                  />
-                </TableCell>
-                <TableCell className="w-full break-all">
-                  <input
-                    type="text"
-                    value={variable.description}
-                    className="w-full bg-transparent outline-none"
-                    placeholder="Enter variable description"
-                    onChange={(e) => update(key, { description: e.target.value })}
-                  />
-                </TableCell>
-                <TableCell className="w-16 text-right">
-                  <div className="flex items-center justify-center gap-2">
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      className="hover:bg-transparent hover:text-[rgba(107,194,224,1)] active:text-[#12B1E7] h-6 w-6"
-                      onClick={() => deleteVariable(key)}
-                    >
-                      <DeleteIcon />
-                    </Button>
-                  </div>
-                </TableCell>
+        <div className="absolute top-16 left-4 bottom-4 right-4">
+          <Table className="table-auto w-full">
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-auto">Key</TableHead>
+                <TableHead className="w-auto">Value</TableHead>
+                <TableHead className="w-full">Description</TableHead>
+                <TableHead className="w-16">{/* Action Column */}</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {Object.entries(variables).map(([key, variable], index) => (
+                <TableRow key={index}>
+                  <TableCell className="w-1/4 break-all">
+                    <input
+                      type="text"
+                      value={key}
+                      className={`w-full bg-transparent outline-none ${key === '' ? 'text-danger' : ''}`}
+                      placeholder="Enter variable key"
+                      onChange={(e) => rename(key, e.target.value)}
+                    />
+                  </TableCell>
+                  <TableCell className="w-1/4 break-all">
+                    <input
+                      type="text"
+                      value={variable.value}
+                      className="w-full bg-transparent outline-none"
+                      placeholder="Enter variable value"
+                      onChange={(e) => update(key, 'value', e.target.value)}
+                    />
+                  </TableCell>
+                  <TableCell className="w-full break-all">
+                    <input
+                      type="text"
+                      value={variable.description}
+                      className="w-full bg-transparent outline-none"
+                      placeholder="Enter variable description"
+                      onChange={(e) => update(key, 'description', e.target.value)}
+                    />
+                  </TableCell>
+                  <TableCell className="w-16 text-right">
+                    <div className="flex items-center justify-center gap-2">
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="hover:bg-transparent hover:text-[rgba(107,194,224,1)] active:text-[#12B1E7] h-6 w-6"
+                        onClick={() => deleteByKey(key)}
+                      >
+                        <DeleteIcon />
+                      </Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
