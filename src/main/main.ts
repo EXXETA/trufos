58ade91a62bd8fd590cdf6972ed5ee3816152ce8
Michael Giek <michael.giek@exxeta.com>
2025-03-11 15:38:41 +0100

    #286 feat: add CreateEnvironmentModal and integrate with NamingModal

--- src/main/main.ts
+++ src/main/main.ts
@@ -28,7 +28,6 @@ const createWindow = async () => {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
-  mainWindow.setFullScreen(true);
 
   // Open links in default browser
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
