e2127373f1b1547bf02846a5f58d07246fa1b344
Eugen Soliar <eugen.soliar@exxeta.com>
2025-05-09 10:58:19 +0200

    #150 - makes sidebar responsive & adds missing icon

--- src/renderer/components/sidebar/SidebarHeaderBar.tsx
+++ src/renderer/components/sidebar/SidebarHeaderBar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { SidebarHeader } from '@/components/ui/sidebar';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { AddIcon } from '@/components/icons';
+import { AddIcon, NewFolderIcon } from '@/components/icons';
 import { NamingModal } from '@/components/sidebar/SidebarRequestList/Nav/Dropdown/modals/NamingModal';
 import { useCollectionStore } from '@/state/collectionStore';
 import { Collection } from 'shim/objects/collection';
@@ -11,42 +11,34 @@ import CollectionDropdown from '@/components/sidebar/CollectionDropdown';
 export const SidebarHeaderBar = () => {
   const collection = useCollectionStore((state) => state.collection);
   const [modalState, setModalState] = useState({ isOpen: false, type: null });
-  const buttonClassName = cn('flex min-w-[24px] h-[36px] p-0 items-center justify-center');
+  const buttonClassName = cn('flex w-full min-w-[24px] h-[36px] items-center justify-center gap-1');
 
   const openModal = (type: 'request' | 'folder') => setModalState({ isOpen: true, type });
 
   return (
     <div>
-      <SidebarHeader>
+      <SidebarHeader className="flex-col gap-4">
         <CollectionDropdown />
 
-        <div className="mt-2 flex w-full max-w-sm items-center space-x-[24px]">
-          <Button
-            className={buttonClassName}
-            type="button"
-            style={{ width: '100%' }}
-            onClick={() => openModal('folder')}
-          >
-            <div className={cn('m-2')}>
-              <AddIcon size={24} color={'black'} />
+        <div className="flex w-full items-center gap-6">
+          <Button className={buttonClassName} type="button" onClick={() => openModal('folder')}>
+            <div className="flex items-center h-6 w-6">
+              <NewFolderIcon size={24} color={'black'} />
             </div>
-            <span className="overflow-hidden">New Folder</span>
+
+            <span className="hidden md:block">New Folder</span>
           </Button>
-          <Button
-            className={buttonClassName}
-            type="button"
-            style={{
-              width: '100%',
-            }}
-            onClick={() => openModal('request')}
-          >
-            <div className={cn('m-2')}>
+
+          <Button className={buttonClassName} type="button" onClick={() => openModal('request')}>
+            <div className="flex items-center h-6 w-6">
               <AddIcon size={24} color={'black'} />
             </div>
-            <span className="overflow-hidden">New Request</span>
+
+            <span className="hidden md:block">New Request</span>
           </Button>
         </div>
       </SidebarHeader>
+
       {modalState.isOpen && (
         <NamingModal
           isOpen={modalState.isOpen}
