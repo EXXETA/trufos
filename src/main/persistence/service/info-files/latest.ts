7363ef81653247d065db840cbe07720c2dc0e933
SoulKa <louiswilke@gmx.de>
2025-05-30 21:49:45 +0200

    #279 - support storing secrets

--- src/main/persistence/service/info-files/latest.ts
+++ src/main/persistence/service/info-files/latest.ts
@@ -4,6 +4,7 @@ import { Collection } from 'shim/objects/collection';
 import { Folder } from 'shim/objects/folder';
 import { TrufosRequest } from 'shim/objects/request';
 import { InfoFile, VERSION, RequestInfoFile, FolderInfoFile, CollectionInfoFile } from './v1-2-0';
+import { VariableMap } from 'shim/objects/variables';
 
 export { VERSION, InfoFile, CollectionInfoFile, FolderInfoFile, RequestInfoFile } from './v1-2-0';
 
@@ -56,4 +57,46 @@ export function fromRequestInfoFile(
   draft: boolean
 ): TrufosRequest {
   return Object.assign(infoFile, { type: 'request' as const, parentId, draft, queryParams: [] });
+}
+
+/**
+ * Extracts secrets from the given variables map and removes them from the original map.
+ * @param variables The variables map to extract secrets from.
+ */
+function extractSecretFromMap(variables: VariableMap) {
+  const secrets: VariableMap = {};
+  for (const [key, variable] of Object.entries(variables)) {
+    if (variable.secret) {
+      secrets[key] = variable;
+      delete variables[key];
+    }
+  }
+  return secrets;
+}
+
+/**
+ * Extracts secrets from the given Trufos object and returns a partial object with secrets removed.
+ * @param object The Trufos object from which to extract secrets. They will be removed from it.
+ */
+export function extractSecrets<T extends TrufosObject>(object: T) {
+  switch (object.type) {
+    case 'folder':
+      return {} as Partial<Folder>;
+    case 'request':
+      return {
+        variables: extractSecretFromMap(object.variables),
+      } as Partial<TrufosRequest>;
+    case 'collection':
+      return {
+        variables: extractSecretFromMap(object.variables),
+        environments: Object.fromEntries(
+          Object.entries(object.environments).map(([key, value]) => [
+            key,
+            {
+              variables: extractSecretFromMap(value.variables),
+            },
+          ])
+        ),
+      } as Partial<Collection>;
+  }
 }
--- src/main/persistence/service/info-files/migrators.ts
+++ src/main/persistence/service/info-files/migrators.ts
@@ -16,15 +16,20 @@ const MIGRATORS = new Map<string, AbstractInfoFileMigrator<VersionedObject, Vers
  * Migrates the given info file to the latest schema and removes the version property.
  * @param infoFile The info file to migrate.
  * @param type The type of the info file.
+ * @param filePath The path to the info file.
  */
 
-export async function migrateInfoFile(infoFile: VersionedObject, type: TrufosObjectType) {
+export async function migrateInfoFile(
+  infoFile: VersionedObject,
+  type: TrufosObjectType,
+  filePath: string
+) {
   while (infoFile.version !== LATEST_VERSION.toString()) {
     logger.debug(`Looking for mapper for source version ${infoFile.version}`);
     const mapper = MIGRATORS.get(infoFile.version);
     if (!mapper) throw new Error(`No mapper found for version ${infoFile.version}`);
     const oldVersion = infoFile.version;
-    infoFile = await mapper.migrate(infoFile, type);
+    infoFile = await mapper.migrate(infoFile, type, filePath);
     logger.info(`Migrated from version ${oldVersion} to ${infoFile.version}`);
     if (oldVersion === infoFile.version) {
       throw new Error(`Mapper for version ${infoFile.version} did not change the version`);
