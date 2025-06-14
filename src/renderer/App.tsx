c3d93bdaf70419873ad8e07627ca51bf7cd5503e
FriedAxons <friedaxons@proton.me>
2025-06-14 07:34:10 -0500

    #391 - Implement resizable sidebar using shadcn components (#396)
    * #391 - Implement resizable sidebar using shadcn components
    
    * #391 - Format resizable.tsx according to prettier
    
    * #391 - Set resizable min widths to pixels and make main body responsive to resize
    
    * #391 - Clarify min size update function name
    
    * #391 - Fix formattting issue
    
    ---------
    
    Co-authored-by: Mike <24782633+Mikerosoft25@users.noreply.github.com>

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,13 +4,45 @@ import { Menubar } from '@/view/Menubar';
 import { RequestWindow } from '@/view/RequestWindow';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import { ResizablePanel, ResizablePanelGroup, ResizableHandle } from '@/components/ui/resizable';
+import { useEffect, useState } from 'react';
+
+const MIN_SIDEBAR_PIXELS = 300;
+const MIN_REQUEST_WINDOW_PIXELS = 500;
 
 export const App = () => {
+  const [minSidebarSize, setMinSidebarSize] = useState(0);
+  const [minRequestWindowSize, setMinRequestWindowSize] = useState(0);
+
+  useEffect(() => {
+    const updateMinResizableSizes = () => {
+      const minSidebarSize = (MIN_SIDEBAR_PIXELS / window.innerWidth) * 100;
+      const minRequestWindowSize = (MIN_REQUEST_WINDOW_PIXELS / window.innerWidth) * 100;
+
+      setMinSidebarSize(minSidebarSize);
+      setMinRequestWindowSize(minRequestWindowSize);
+    };
+
+    updateMinResizableSizes();
+    window.addEventListener('resize', updateMinResizableSizes);
+
+    return () => {
+      window.removeEventListener('resize', updateMinResizableSizes);
+    };
+  }, []);
+
   return (
     <TooltipProvider delayDuration={750}>
-      <SidebarProvider className="grid grid-cols-[1fr_3fr]">
-        <Menubar />
-        <RequestWindow />
+      <SidebarProvider className="grid">
+        <ResizablePanelGroup direction="horizontal" className="h-full w-full">
+          <ResizablePanel defaultSize={minSidebarSize} minSize={minSidebarSize}>
+            <Menubar />
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel minSize={minRequestWindowSize}>
+            <RequestWindow />
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </SidebarProvider>
     </TooltipProvider>
   );
