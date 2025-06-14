faf58a7d77bbd8ab183b567adeba2349b7dd1191
Mike <24782633+Mikerosoft25@users.noreply.github.com>
2025-06-14 12:30:31 +0200

    #391 - Set resizable min widths to pixels and make main body responsive to resize

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,17 +5,41 @@ import { RequestWindow } from '@/view/RequestWindow';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { ResizablePanel, ResizablePanelGroup, ResizableHandle } from '@/components/ui/resizable';
+import { useEffect, useState } from 'react';
+
+const MIN_SIDEBAR_PIXELS = 300;
+const MIN_REQUEST_WINDOW_PIXELS = 500;
 
 export const App = () => {
+  const [minSidebarSize, setMinSidebarSize] = useState(0);
+  const [minRequestWindowSize, setMinRequestWindowSize] = useState(0);
+
+  useEffect(() => {
+    const updateMinSidebarSize = () => {
+      const minSidebarSize = (MIN_SIDEBAR_PIXELS / window.innerWidth) * 100;
+      const minRequestWindowSize = (MIN_REQUEST_WINDOW_PIXELS / window.innerWidth) * 100;
+
+      setMinSidebarSize(minSidebarSize);
+      setMinRequestWindowSize(minRequestWindowSize);
+    };
+
+    updateMinSidebarSize();
+    window.addEventListener('resize', updateMinSidebarSize);
+
+    return () => {
+      window.removeEventListener('resize', updateMinSidebarSize);
+    };
+  }, []);
+
   return (
     <TooltipProvider delayDuration={750}>
       <SidebarProvider className="grid">
         <ResizablePanelGroup direction="horizontal" className="h-full w-full">
-          <ResizablePanel defaultSize={25} minSize={25}>
+          <ResizablePanel defaultSize={minSidebarSize} minSize={minSidebarSize}>
             <Menubar />
           </ResizablePanel>
           <ResizableHandle />
-          <ResizablePanel defaultSize={75}>
+          <ResizablePanel minSize={minRequestWindowSize}>
             <RequestWindow />
           </ResizablePanel>
         </ResizablePanelGroup>
--- src/renderer/components/mainWindow/MainBody.tsx
+++ src/renderer/components/mainWindow/MainBody.tsx
@@ -3,7 +3,7 @@ import { OutputTabs } from '@/components/mainWindow/bodyTabs/OutputTabs';
 
 export function MainBody() {
   return (
-    <div className="grid grid-rows-[48%_48%] xl:grid-cols-2 xl:grid-rows-1 h-full gap-6">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(450px,1fr))] h-full gap-6">
       <InputTabs className="min-h-0 min-w-0" />
       <OutputTabs className="min-h-0 min-w-0" />
     </div>
