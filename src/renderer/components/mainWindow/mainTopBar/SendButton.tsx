c383940c577206e25b99e6eace114044aaa17399
Ladzsz <londonmason602@gail.com>
2025-06-26 18:38:56 -0400

    Add loading indicator to SendButton

--- src/renderer/components/mainWindow/mainTopBar/SendButton.tsx
+++ src/renderer/components/mainWindow/mainTopBar/SendButton.tsx
@@ -4,12 +4,14 @@ import { ArrowForwardIcon } from '@/components/icons';
 
 interface SendButtonProps {
   onClick: () => void;
+  disabled?: boolean;
+  children?: React.ReactNode;
 }
 
-export const SendButton: React.FC<SendButtonProps> = ({ onClick }) => (
-  <Button className="gap-3 pl-[30px]" onClick={onClick} variant="secondary">
+export const SendButton: React.FC<SendButtonProps> = ({ onClick, disabled = false, children }) => (
+  <Button className="gap-3 pl-[30px]" onClick={onClick} variant="secondary" disabled={disabled}>
     <span className="font-bold leading-4">Send</span>
 
-    <ArrowForwardIcon />
+    {children ?? <ArrowForwardIcon />}
   </Button>
 );
