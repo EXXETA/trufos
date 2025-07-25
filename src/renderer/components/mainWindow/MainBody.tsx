66b426ec632503a61febcde1a886072b6ba0c5cf
Ladzsz <londonmason602@gail.com>
2025-07-25 11:17:05 -0400

    fixed wobble issue

--- src/renderer/components/mainWindow/MainBody.tsx
+++ src/renderer/components/mainWindow/MainBody.tsx
@@ -3,7 +3,7 @@ import { OutputTabs } from '@/components/mainWindow/bodyTabs/OutputTabs';
 
 export function MainBody() {
   return (
-    <div className="grid h-full grid-cols-[repeat(auto-fit,minmax(450px,1fr))] gap-6">
+    <div className="grid h-full grid-rows-[48%_48%] gap-6 xl:grid-cols-2 xl:grid-rows-1">
       <InputTabs className="min-h-0 min-w-0" />
       <OutputTabs className="min-h-0 min-w-0" />
     </div>
