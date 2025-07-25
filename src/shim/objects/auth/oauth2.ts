37e6f36795f9ad647f9be1aecc13721f5cc32508
SoulKa <louiswile@gmx.de>
2025-07-25 22:13:21 +0200

    #460 - add auth code flow in backend

--- src/shim/objects/auth/oauth2.ts
+++ src/shim/objects/auth/oauth2.ts
@@ -4,6 +4,7 @@ import { AuthorizationType } from '.';
 export enum OAuth2Method {
   CLIENT_CREDENTIALS = 'client-credentials',
   AUTHORIZATION_CODE = 'authorization-code',
+  AUTHORIZATION_CODE_PKCE = 'authorization-code-pkce',
 }
 
 export enum OAuth2ClientAuthenticationMethod {
@@ -11,9 +12,9 @@ export enum OAuth2ClientAuthenticationMethod {
   REQUEST_BODY = 'request-body',
 }
 
-export type OAuth2ClientCrentialsAuthorizationInformation = {
+export type Oauth2BaseAuthorizationInformation = {
   type: AuthorizationType.OAUTH2;
-  method: OAuth2Method.CLIENT_CREDENTIALS;
+  method: OAuth2Method;
   clientId: string;
   clientSecret: string;
   tokenUrl: string;
@@ -24,4 +25,32 @@ export type OAuth2ClientCrentialsAuthorizationInformation = {
   tokens?: TokenEndpointResponse;
 };
 
-export type OAuth2AuthorizationInformation = OAuth2ClientCrentialsAuthorizationInformation;
+export type OAuth2ClientCrentialsAuthorizationInformation = Oauth2BaseAuthorizationInformation & {
+  method: OAuth2Method.CLIENT_CREDENTIALS;
+};
+
+export type OAuth2ClientAuthorizationCodeFlowInformation = Oauth2BaseAuthorizationInformation & {
+  method: OAuth2Method.AUTHORIZATION_CODE;
+  authorizationUrl: string;
+  redirectUri: string;
+  state?: string; // generated if not provided
+};
+
+export enum OAuth2PKCECodeChallengeMethod {
+  S256 = 'S256',
+  PLAIN = 'plain',
+}
+
+export type OAuth2ClientAuthorizationCodeFlowPKCEInformation =
+  OAuth2ClientAuthorizationCodeFlowInformation & {
+    method: OAuth2Method.AUTHORIZATION_CODE_PKCE;
+    authorizationUrl: string;
+    redirectUri: string;
+    codeChallengeMethod: OAuth2PKCECodeChallengeMethod;
+    codeVerifier?: string; // generated if not provided
+  };
+
+export type OAuth2AuthorizationInformation =
+  | OAuth2ClientCrentialsAuthorizationInformation
+  | OAuth2ClientAuthorizationCodeFlowInformation
+  | OAuth2ClientAuthorizationCodeFlowPKCEInformation;
