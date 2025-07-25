5f7bb77f43ffef89d8416c224d124a51ecc11955
SoulKa <louiswilke@gmx.de>
2025-07-25 22:58:38 +0200

    #460 - implement auth code flow

--- src/main/network/authentication/oauth2/auth-code-flow.ts
+++ src/main/network/authentication/oauth2/auth-code-flow.ts
@@ -5,29 +5,50 @@ import { BrowserWindow } from 'electron';
 import { once } from 'node:events';
 
 export default class AuthCodeFlowAuthorizationStrategy extends OAuth2AuthStrategy<OAuth2ClientAuthorizationCodeFlowInformation> {
-  private async getAuthorizationCode(url: URL) {
+  private async getAuthorizationCode(url: URL, callbackUrl: string) {
+    let redirectUrl: URL | undefined;
+
     // Create the browser window.
     const window = new BrowserWindow({
       frame: false,
       titleBarStyle: 'hidden',
     });
 
+    // intercept the callback URL to get the auth code
+    window.webContents.session.webRequest.onBeforeRequest(
+      { urls: [callbackUrl + '*'] },
+      (details, callback) => {
+        if (details.method.toUpperCase() === 'GET') {
+          logger.info('Completed auth code login');
+          callback({ cancel: true });
+          redirectUrl = URL.parse(details.url);
+          window.close();
+        }
+      }
+    );
+
+    logger.info(`Opening window with authorization URL: ${url}`);
     window.loadURL(url.toString());
     await once(window, 'close');
+    return redirectUrl;
   }
 
   protected async getTokens() {
+    // prepare the authorization URL and state
     const configuration = this.getConfiguration();
     const parameters = this.getParameters();
     parameters.redirect_uri = this.authInfo.redirectUri;
     parameters.state = this.authInfo.state ?? randomState();
+    const url = buildAuthorizationUrl(configuration, parameters);
 
-    const redirectUrl = buildAuthorizationUrl(configuration, parameters);
-    logger.info(`Redirecting to authorization URL: ${redirectUrl}`);
+    // open browser window to get the authorization code
+    const authCode = await this.getAuthorizationCode(url, parameters.redirect_uri);
+    if (!authCode) {
+      throw new Error('Authorization code not received');
+    }
 
-    await this.getAuthorizationCode(redirectUrl);
-
-    this.authInfo.tokens = await authorizationCodeGrant(configuration, redirectUrl, {
+    // exchange the authorization code for tokens
+    this.authInfo.tokens = await authorizationCodeGrant(configuration, authCode, {
       expectedState: parameters.state,
     });
   }
--- src/main/network/authentication/oauth2/strategy-factory.ts
+++ src/main/network/authentication/oauth2/strategy-factory.ts
@@ -1,11 +1,15 @@
 import { OAuth2AuthorizationInformation, OAuth2Method } from 'shim/objects/auth/oauth2';
 import ClientCredentialsAuthorizationStrategy from './client-credentials';
+import AuthCodeFlowAuthorizationStrategy from './auth-code-flow';
 
 export function createOAuth2Strategy(auth: OAuth2AuthorizationInformation) {
   switch (auth.method) {
     case OAuth2Method.CLIENT_CREDENTIALS:
       return new ClientCredentialsAuthorizationStrategy(auth);
+    case OAuth2Method.AUTHORIZATION_CODE:
+      return new AuthCodeFlowAuthorizationStrategy(auth);
     default:
+      // @ts-expect-error should never happen
       throw new Error(`Unsupported OAuth2 method: ${auth.method}`);
   }
 }
