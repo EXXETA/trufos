820f3e6d6928460c56af2ce1069c440ae189793a
SoulKa <louiswilke@gmx.de>
2025-09-25 09:46:15 +0200

    #498 - support importing collections in backend

--- src/shim/fs.ts
+++ src/shim/fs.ts
@@ -7,3 +7,15 @@ export type FileInfo = {
   isFile: boolean;
   isDirectory: boolean;
 };
+
+/**
+ * Sanitize a title by removing invalid characters and formatting it. The resulting string may be used as file or directory name
+ * @param title the title to sanitize
+ * @returns the sanitized title
+ */
+export function sanitizeTitle(title: string): string {
+  return title
+    .toLowerCase()
+    .replace(/\s/g, '-')
+    .replace(/[^a-z0-9-]/g, '');
+}
