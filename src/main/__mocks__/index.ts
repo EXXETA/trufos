4b170c00c115eda1ed8e2e289801ed82b880f558
SoulKa <louiswilke@gmx.de>
2025-12-11 16:13:00 +0100

    #422 - add tests

--- src/main/__mocks__/index.ts
+++ src/main/__mocks__/index.ts
@@ -1,7 +1,7 @@
 import { homedir, tmpdir } from 'node:os';
 import { vol } from 'memfs';
 import { vi, beforeEach, expect } from 'vitest';
-import { prettifyError, ZodSafeParseResult } from 'zod';
+import { prettifyError, ZodSafeParseResult, ZodType, ZodTypeAny } from 'zod';
 
 // @ts-expect-error mock global logger with console
 global.logger = console;
@@ -21,19 +21,31 @@ beforeEach(() => {
 });
 
 expect.extend({
-  toBeZodSuccess<T>(result: ZodSafeParseResult<T>) {
+  toBeOfSchema(received: unknown, schema: ZodTypeAny) {
+    const result = schema.safeParse(received);
     return {
       pass: result.success,
       message: () =>
         result.success
-          ? 'Expected Zod parse to fail, but it succeeded.'
+          ? 'Expected value to not match schema, but it did.'
+          : prettifyError(result.error),
+    };
+  },
+  async toBeOfSchemaAsync(received: unknown, schema: ZodTypeAny) {
+    const result = await schema.safeParseAsync(received);
+    return {
+      pass: result.success,
+      message: () =>
+        result.success
+          ? 'Expected value to not match schema, but it did.'
           : prettifyError(result.error),
     };
   },
 });
 
 declare module 'vitest' {
-  interface Assertion<T = any> {
-    toBeZodSuccess(): T;
+  interface Assertion {
+    toBeOfSchema(schema: ZodTypeAny): void;
+    toBeOfSchemaAsync(schema: ZodTypeAny): Promise<void>;
   }
 }
