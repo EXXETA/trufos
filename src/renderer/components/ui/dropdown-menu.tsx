2e34f92fa238e28f063985c0d9a21d298380846f
anwu <andrin.wulz@exxeta.ch>
2026-02-05 11:23:57 +0100

    #629 - cursor-pointer globally defined in dropdown-menu.tsx

--- src/renderer/components/ui/dropdown-menu.tsx
+++ src/renderer/components/ui/dropdown-menu.tsx
@@ -90,7 +90,7 @@ const DropdownMenuItem = React.forwardRef<
   <DropdownMenuPrimitive.Item
     ref={ref}
     className={cn(
-      'focus:bg-accent focus:text-accent-foreground relative flex cursor-default items-center gap-2 px-2 py-1.5 text-sm outline-hidden transition-colors select-none data-disabled:pointer-events-none data-disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+      'focus:bg-accent focus:text-accent-foreground relative flex cursor-pointer items-center gap-2 px-2 py-1.5 text-sm outline-hidden transition-colors select-none data-disabled:pointer-events-none data-disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
       'data-[highlighted]:bg-divider data-[highlighted]:text-accent-foreground',
       inset && 'pl-8',
       className
