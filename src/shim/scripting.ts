0e936ba75c9087fe183b3200829d1c520e1139e5
SoulKa <louiswilke@gmx.de>
2026-02-23 14:37:02 +0100

    feat: enhance documentation for collection and environment variable methods in GlobalScriptingApi

--- src/shim/scripting.ts
+++ src/shim/scripting.ts
@@ -13,12 +13,54 @@ export interface GlobalScriptingApi {
     /** The current version of the app. */
     readonly version: string;
 
+    /**
+     * Get a variable from the current collection.
+     * @param name The name of the variable.
+     * @returns The current value of the variable, or `undefined` if it doesn't exist.
+     */
     getCollectionVariable(name: string): string | undefined;
 
+    /**
+     * Set a variable in the current collection. If the variable doesn't exist, it will be created.
+     * @param name The name of the variable.
+     * @param value The value to set for the variable. Can be a string or an object with additional properties.
+     * @example
+     * // Set a simple string variable
+     * trufos.setCollectionVariable('apiUrl', 'https://api.example.com');
+     *
+     * // Set a variable with additional properties
+     * trufos.setCollectionVariable('userToken', {
+     *   value: 'abc123',
+     *   description: 'Token for authenticating API requests',
+     *   secret: true, // does not appear in git and is stored encrypted
+     * });
+     */
     setCollectionVariable(name: string, value: string | VariableObject): void;
 
+    /**
+     * Get a variable from an environment.
+     * @param environment The name of the environment to get the variable from, or `undefined` to get it from the currently selected environment.
+     * @param name The name of the variable.
+     * @returns The current value of the variable, or `undefined` if it doesn't exist.
+     */
     getEnvironmentVariable(environment: string | undefined, name: string): string | undefined;
 
+    /**
+     * Set a variable in an environment. If the variable doesn't exist, it will be created.
+     * @param environment The name of the environment to set the variable in, or `undefined` to set it in the currently selected environment.
+     * @param name The name of the variable.
+     * @param value The value to set for the variable. Can be a string or an object with additional properties.
+     * @example
+     * // Set a simple string variable in the current environment
+     * trufos.setEnvironmentVariable(undefined, 'apiUrl', 'https://api.example.com');
+     *
+     * // Set a variable with additional properties in a specific environment
+     * trufos.setEnvironmentVariable('staging', 'userToken', {
+     *   value: 'abc123',
+     *   description: 'Token for authenticating API requests in staging environment',
+     *   secret: true, // does not appear in git and is stored encrypted
+     * });
+     */
     setEnvironmentVariable(
       environment: string | undefined,
       name: string,
