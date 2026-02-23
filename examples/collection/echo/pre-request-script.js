768df1bb7671b8313923909265370b6bc51c65fa
SoulKa <louiswilke@gmx.de>
2026-02-23 13:26:50 +0100

    chore: simplify example script

--- examples/collection/echo/pre-request-script.js
+++ examples/collection/echo/pre-request-script.js
@@ -1,8 +1,3 @@
-const variable = 'counter';
-let value = trufos.getCollectionVariable(variable);
-console.log(`Current value of ${variable}:`, value);
-value = parseInt(value || '0');
-console.log(`Parsed value of ${variable}:`, value);
-value++;
-console.log(`Incremented value of ${variable}:`, value);
-trufos.setCollectionVariable(variable, value.toString());
+const key = 'counter';
+const value = parseInt(trufos.getCollectionVariable(key) || '0');
+trufos.setCollectionVariable(key, (value + 1).toString());
