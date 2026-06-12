a1bc8b3716e34795c404202c1fb941b1cea3fe57
euso <eugen.soliar@exxeta.com>
2026-06-12 15:58:44 +0200

    #446 - gets rid of unused import

--- src/renderer/components/mainWindow/bodyTabs/OutputTabs.tsx
+++ src/renderer/components/mainWindow/bodyTabs/OutputTabs.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import {
   Table,
