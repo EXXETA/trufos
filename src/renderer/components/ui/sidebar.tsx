9d87daf4316b0e9e4e30e405bb663cfd02b5d301
Kevin Werber <kevin.werber@exxeta.com>
2026-08-27 10:14:53 +0200

    #1001 - Extend footer gap to from gap-2 to gap-6
    According to the penpot design, the gap between
    the footer divider and the footer contents should
    be gap-6 instead of gap-2.

--- src/renderer/components/ui/sidebar.tsx
+++ src/renderer/components/ui/sidebar.tsx
@@ -321,7 +321,7 @@ const SidebarFooter = React.forwardRef<HTMLDivElement, React.ComponentProps<'div
       <div
         ref={ref}
         data-sidebar="footer"
-        className={cn('flex flex-col gap-2', className)}
+        className={cn('flex flex-col gap-6', className)}
         {...props}
       />
     );
